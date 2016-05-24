56319cf6c08d8ca6aba22487c725e51c121c11bd
paulwallis42 <paulwallis42@gmail.com>
2016-05-24 15:44:49 +0100

    CRU all working D is next

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,11 +9,26 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name)
+    params.require(:restaurant).permit(:name, :description)
   end
 
   def create
     Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
     redirect_to '/restaurants'
   end
 
